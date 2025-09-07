da8514d0de8601823086fdc25351ddeb59225ee8
Guih033 <119979086+Guih033@users.noreply.github.com>
2025-09-07 00:00:58 -0400

    Update config.js

--- Oliveira-AI-website/components/utils/config.js
+++ Oliveira-AI-website/components/utils/config.js
@@ -1,2 +1,3 @@
-export const CRM_ENDPOINT_URL = ""; // Paste your Google Apps Script Web App URL here
+export const CRM_ENDPOINT_URL = "https://script.google.com/macros/s/AKfycbxFv7MHtcZFXz9w5yYe5U6YCj6pAfWGU3beZgRRk3fGKetrSUyBcEbAa3mHpOxLgxzt/exec"; // Paste your Google Apps Script Web App URL here
+
 export const RECAPTCHA_V3_SITE_KEY = ""; // Optional: reCAPTCHA v3 site key
